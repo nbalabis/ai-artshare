b54cfeb687ad6ae2ac7763f189cd568e180de6b7
nbalabis <88114907+nbalabis@users.noreply.github.com>
2023-04-21 15:23:49 -0700

    Remove testing lines

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,7 +4,6 @@ import { download } from "../assets";
 import { downloadImage } from "../utils";
 
 const Card = ({ _id, name, prompt, photo }) => {
-  console.log(prompt);
   return (
     <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
       <img
