e7932a16f9b53e978e95140d41a1ceaab7a81b00
nbalabis <88114907+nbalabis@users.noreply.github.com>
2023-04-21 11:42:31 -0700

    Add temporary example logic to prevent overspending budget while testing

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
+import exampleImg from './example.webp'
 
 export {
   download,
   logo,
   preview,
+  exampleImg
 };
