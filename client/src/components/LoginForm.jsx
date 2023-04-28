dc1c6b9e0405df0d6fb55989aa2d45e890630e3a
nbalabis <88114907+nbalabis@users.noreply.github.com>
2023-04-27 22:25:21 -0700

    Create basic login form

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { FormField } from "../components";
 
 const LoginForm = () => {
-  return (
-    <div>LoginForm</div>
-  )
-}
+  const [user, setUser] = useState({ email: "", password: "" });
 
-export default LoginForm
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    console.log(user)
+  };
+
+  return (
+    <div>
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Email"
+            type="email"
+            name="email"
+            vlaue={user.email}
+            handleChange={handleChange}
+            required
+          />
+          <FormField
+            labelName="Password"
+            type="password"
+            name="password"
+            vlaue={user.password}
+            handleChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
