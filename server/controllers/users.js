a587d37a127dfca8d616d9b9a3f2f715f31d1631
nbalabis <88114907+nbalabis@users.noreply.github.com>
2023-05-06 00:15:57 -0700

    Log user in after successful registration

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,16 +2,15 @@ import User from "../mongodb/models/user.js";
 
 const register = async (req, res) => {
     try {
-        // Assuming the form data is sent as JSON in the request body
         const { username, password } = req.body;
         const user = new User({ username })
         const registeredUser = await User.register(user, password)
 
-        //TODO: LOG USER IN
-
-        // Send a response with a success message
-        res.status(200).json({ message: 'Account successfully created. Welcome to AI-ArtShare!' })
-
+        req.login(registeredUser, err => {
+            if (err) return next(err)
+            res.status(200).json({ message: 'Account successfully created. Welcome to AI-ArtShare!' })
+            console.log('Auth Successful')
+        })
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import session from 'express-session';
 
+import User from './mongodb/models/user.js';
 import connectDB from './mongodb/connect.js';
 import userRoutes from './routes/userRoutes.js'
 import postRoutes from './routes/postRoutes.js';
@@ -12,6 +16,31 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+// Configure the local authentication strategy
+passport.use(new LocalStrategy(User.authenticate()));
+
+// Configure the Passport.js user serialization and deserialization functions
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Create a session for the authenticated user
+const sessionConfig = {
+  secret: 'your secret key here',
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(session(sessionConfig));
+
+// Initialize Passport.js and restore authentication state, if any, from the session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Middleware that adds the current authenticated user object as a `user` property on the `req` object
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use('/', userRoutes)
 app.use('/api/v1/post', postRoutes);
